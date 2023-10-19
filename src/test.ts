1f5b69d353d496b00588a6dbce0457c8747ca681
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-19 14:23:55 -0500

    Checkpoint

--- src/test.ts
+++ src/test.ts
@@ -1,45 +1,69 @@
 import ts from 'typescript';
+import path from 'path';
+
+import { readFileSync } from 'fs';
+
+const libsDTS = readFileSync(path.resolve('./node_modules/typescript/lib/lib.d.ts'), 'utf8').toString();
 
 function typecheck(code: string) {
 	console.log(`Type checking the following code:\n${code}\n`);
-	// Create a SourceFile from TypeScript code
-	const sourceFile = ts.createSourceFile('test.ts', code, ts.ScriptTarget.ESNext, true, ts.ScriptKind.TS);
 
-	sourceFile.forEachChild((node) => console.log("flag", node.kind));
+	const libs = ts.createSourceFile(
+		'libs.d.ts',
+		libsDTS,
+		ts.ScriptTarget.ESNext,
+	);
 
-	const program = ts.createProgram({
-		rootNames: [sourceFile.fileName],
-		options: {
-			esModuleInterop: true,
+	const file = ts.createSourceFile('index.ts', code, ts.ScriptTarget.ESNext);
+
+	// This is needed
+	const compilerHost: ts.CompilerHost = {
+		fileExists: (fileName) => fileName === file.fileName,
+		getSourceFile: (fileName) => (fileName === file.fileName ? file : undefined),
+		getDefaultLibFileName: () => 'lib.d.ts',
+		writeFile: () => {},
+		getCurrentDirectory: () => '/',
+		getDirectories: () => [],
+		getCanonicalFileName: (f) => f.toLowerCase(),
+		getNewLine: () => '\n',
+		useCaseSensitiveFileNames: () => false,
+		readFile: (fileName) => (fileName === file.fileName ? file.text : undefined),
+		resolveModuleNames: () => [],
+	};
+
+	const program = ts.createProgram(
+		[file.fileName],
+		{
+			strict: true,
 			module: ts.ModuleKind.ESNext,
+			moduleResolution: ts.ModuleResolutionKind.NodeNext,
 			target: ts.ScriptTarget.ESNext,
 		},
+		compilerHost,
+	);
+
+	const emitResult = program.emit();
+	const allDiagnostics = ts.getPreEmitDiagnostics(program);
+
+	allDiagnostics.forEach((diagnostic) => {
+		if (diagnostic.file) {
+			const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start!);
+			const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
+			console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+		} else {
+			console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
+		}
 	});
 
-	// Get diagnostics (errors) for the source file
-	const diagnostics = ts.getPreEmitDiagnostics(program, sourceFile);
-
-	// Check if there are any errors
-	if (diagnostics.length > 0) {
-		console.log('Errors found:');
-		diagnostics.forEach((diagnostic) => {
-			if (diagnostic.file) {
-				const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(diagnostic.start!);
-				const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n');
-				console.log(`Error at ${line + 1}, character ${character + 1}: ${message}`);
-			} else {
-				console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, '\n'));
-			}
-		});
-	} else {
-		console.log('No errors found.');
-	}
+	const exitCode = emitResult.emitSkipped ? 1 : 0;
+	console.log(`Process exiting with code '${exitCode}'.`);
+	process.exit(exitCode);
 }
 
 try {
-	const CODE = ``;
+	const CODE = 'const a: string = 1;';
 	console.log(typecheck(CODE));
-	// eslint-disable-next-line 
+	// eslint-disable-next-line
 } catch (err: any) {
 	console.log(err.message);
 }
